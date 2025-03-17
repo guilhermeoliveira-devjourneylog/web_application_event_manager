754cb068b813aa68151291071e03bb878c5a5029
Guilherme Oliveira <dev_guilherme_oliveira@outlook.com>
2025-03-17 00:20:10 +0000

    Initial Structure

---
+++ EventManager/Models/EventModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EventManager.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
