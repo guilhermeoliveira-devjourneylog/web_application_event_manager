602b38632962215ac2d14c9e6f7bbe9e415600b9
Guilherme Oliveira <dev_guilherme_oliveira@outlook.com>
2025-03-18 04:45:29 +0000

    resolve EditForm binding and UserSessionService

--- EventManager/Program.cs
+++ EventManager/Program.cs
@@ -1,4 +1,6 @@
-using EventManager.Components;
+﻿using EventManager.Components;
+using EventManager.Services;
+using EventManager.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,18 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Corrigido para Scoped (era Singleton antes, o que causava o erro)
+builder.Services.AddScoped<UserSessionService>();
+builder.Services.AddScoped<AttendanceService>();
+builder.Services.AddSingleton<EventData>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
